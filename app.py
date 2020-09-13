cd3ff74f1c3f7edad79b55a7b20f4f81f636d63f
Clare <clare.young07@gmail.com>
2020-09-13 14:40:19 +0000

    First template test

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'MilestoneProjectThree'
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello'
+@app.route('/get_recipe')
+def get_recipe():
+    return render_template("recipe.html", tasks=mongo.db.tasks.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
