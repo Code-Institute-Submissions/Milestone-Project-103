202b73195df6978dbe626b5d8214d9e463e36618
Clare <clare.young07@gmail.com>
2020-09-13 13:55:12 +0000

    Updated environment variables for Mongodb

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask
 
+if os.path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 @app.route('/')
 def hello():
