680914615123349632d3de711c513b967e842f2a
Clare <clare.young07@gmail.com>
2020-09-15 12:07:59 +0000

    Add recipe form and functionality written

--- app.py
+++ app.py
@@ -18,9 +18,20 @@ mongo = PyMongo(app)
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.Recipes.find())
 
+
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template('addrecipe.html')
+    return render_template('addrecipe.html',
+    categories=mongo.db.Categories.find())
+
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.Recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
