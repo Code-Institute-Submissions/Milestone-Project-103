f03c673ca434e3d8b3b109e7768524dc6944a12d
Clare <clare.young07@gmail.com>
2020-09-25 17:04:28 +0000

    Tidied app comments

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 @app.route('/find_recipe')
 def find_recipe():
@@ -21,28 +22,31 @@ def find_recipe():
     categories = mongo.db.Categories.find()
     return render_template('contents.html', recipes=recipes, categories=categories)
 
-""" View all recipes """
+
 @app.route('/get_recipes')
 def get_recipes():
+    """ View all recipes """
     return render_template("recipes.html", recipes=mongo.db.Recipes.find())
 
-""" View an individual recipe """
+
 @app.route('/view_recipe/<recipe_id>')
 def view_recipe(recipe_id):
+    """ View an individual recipe """
     the_recipe = mongo.db.Recipes.find_one({"_id": ObjectId(recipe_id)})
     all_categories = mongo.db.Categories.find()
     return render_template('singlerecipe.html', recipe=the_recipe, categories=all_categories)
 
-""" Add a recipe """
+
 @app.route('/add_recipe')
 def add_recipe():
+    """ Add a recipe """
     return render_template('addrecipe.html',
         categories=mongo.db.Categories.find())
 
-""" Insert the recipe to the database and redirect the user to the View all recipes page """
+
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-
+    """ Insert the recipe to the database and redirect the user to the View all recipes page """
     ingredientslist = request.form.getlist('ingredients[]')
     if "" in ingredientslist:
         ingredientslist.remove("")
@@ -71,18 +75,19 @@ def insert_recipe():
     recipes.insert_one(recipe_data)
     return redirect(url_for('get_recipes'))
 
-""" Edit a recipe """
+
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
+    """ Edit a recipe """
     the_recipe = mongo.db.Recipes.find_one({"_id": ObjectId(recipe_id)})
     all_categories = mongo.db.Categories.find()
     return render_template('editrecipe.html', recipe=the_recipe, categories=all_categories)
 
-""" Insert the edited recipe into the database and redirect the site user to the View all recipes page """
+
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
 def update_recipe(recipe_id):
+    """ Insert the edited recipe into the database and redirect the site user to the View all recipes page """
     recipes = mongo.db.Recipes
-
     ingredientslist = request.form.getlist('ingredients[]')
     if "" in ingredientslist:
         ingredientslist.remove("")
@@ -97,7 +102,6 @@ def update_recipe(recipe_id):
     methods = methodslist
     image = request.form['image']
 
-
     recipes.update({'_id': ObjectId(recipe_id)},
     {
         'recipe_name' : recipe_name,
@@ -109,28 +113,32 @@ def update_recipe(recipe_id):
         'image' : image
     })
     return redirect(url_for('get_recipes'))
-    
-""" Delete a recipe and redirect the user to the View all recipes page """
+
+
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
+    """ Delete a recipe and redirect the user to the View all recipes page """
     mongo.db.Recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_recipes'))
 
-""" Retrieve all categories with a view to editing or deleting them """
+
 @app.route('/get_categories')
 def get_categories():
+    """ Retrieve all categories with a view to editing or deleting them """
     return render_template('categories.html',
     categories=mongo.db.Categories.find())
 
-""" Edit a category """
+
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
+    """ Edit a category """
     return render_template('editcategory.html', 
     category = mongo.db.Categories.find_one({'_id': ObjectId(category_id)}))
 
-""" Insert the updated category into the database and redirect the user to the categories list page """
+
 @app.route('/update_category/<category_id>', methods=["POST"])
 def update_category(category_id):
+    """ Insert the updated category into the database and redirect the user to the categories list page """
     categories = mongo.db.Categories
     category_name = request.form['category_name']
 
@@ -140,28 +148,31 @@ def update_category(category_id):
     })
     return redirect(url_for('get_categories'))
 
-""" Delete a category and redirect the user to the categories list page """
+
 @app.route('/delete_category/<category_id>')
 def delete_category(category_id):
+    """ Delete a category and redirect the user to the categories list page """
     mongo.db.Categories.remove({'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
 
-""" Add a category """
+
 @app.route('/add_category')
 def add_category():
+    """ Add a category """
     return render_template('addcategory.html')
 
-""" Insert the new category into the database and redirect the user to the categories list page """
+
 @app.route('/insert_category', methods=['POST'])
 def insert_category():
+    """ Insert the new category into the database and redirect the user to the categories list page """
     category = mongo.db.Categories
     category_document = {'category_name': request.form.get('category_name')}
     category.insert_one(category_document)
     return redirect(url_for('get_categories'))
+
 
 """ Run the application """
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
     debug=True)
-
